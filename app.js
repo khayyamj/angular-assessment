d162902261481af4a43a141f2defd418d23a8d9b
Khayyam Jones <Khayyam.Jones@gmail.com>
2017-01-27 10:06:00 -0700

    router set up; step 2 complete

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 angular.module('assessment',['ui.router'])
 .config(function($stateProvider, $urlRouterProvider) {
 
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: './views/home.html'
+        })
+        .state('about', {
+            url: '/about',
+            templateUrl: './views/about.html'
+        })
+        .state('blog', {
+            url: '/blog',
+            templateUrl: './views/blog.html'
+        })
+        .state('shop', {
+            url: '/shop',
+            templateUrl: './views/shop.html'
+        });
 
+    $urlRouterProvider.otherwise('/');
 
 });
